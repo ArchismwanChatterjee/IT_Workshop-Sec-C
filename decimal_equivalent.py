849703cdcd1906cd75f868232f4b9378388d9c1f
Archismwan Chatterjee <115975340+ArchismwanChatterjee@users.noreply.github.com>
2023-08-05 01:55:26 +0530

    Add files via upload

---
+++ decimal_equivalent.py
@@ -0,0 +1,17 @@
+a=2
+for i in range(9):
+    print("The decimal equivalent is ",1/a)
+    a=a+1
+
+'''
+The decimal equivalent is  0.5
+The decimal equivalent is  0.3333333333333333
+The decimal equivalent is  0.25
+The decimal equivalent is  0.2
+The decimal equivalent is  0.16666666666666666
+The decimal equivalent is  0.14285714285714285
+The decimal equivalent is  0.125
+The decimal equivalent is  0.1111111111111111
+The decimal equivalent is  0.1
+
+'''
